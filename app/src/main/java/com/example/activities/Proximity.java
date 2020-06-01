5133c09c65a8be0795224e5e9dd2bce8df69f8e5
germanleonelvianna <germanvianna95@gmail.com>
2020-06-01 11:54:23 -0300

    creadas nuevas activities, accelerometer, proximity y events

---
+++ app/src/main/java/com/example/activities/Proximity.java
@@ -0,0 +1,18 @@
+package com.example.activities;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.sensores.R;
+
+public class Proximity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proximity);
+    }
+
+}
