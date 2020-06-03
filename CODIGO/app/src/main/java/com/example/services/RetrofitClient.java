2272a7654f1251b8d33f1721d75dd284f3b0489e
germanleonelvianna <germanvianna95@gmail.com>
2020-06-03 19:44:48 -0300

    deteccion de internet, creacion de clases ServiceManager y NetworkState para la manipulacion de broadcast, modificacion de estilo de activity_events

--- CODIGO/app/src/main/java/com/example/services/RetrofitClient.java
+++ CODIGO/app/src/main/java/com/example/services/RetrofitClient.java
@@ -1,5 +1,8 @@
 package com.example.services;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -8,11 +11,16 @@ public class RetrofitClient {
     private static final String URL = "http://so-unlam.net.ar/api/api/";
     private static RetrofitClient instanceRetrofit;
     private Retrofit retrofit;
+    final OkHttpClient okHttpClient = new OkHttpClient.Builder()
+            .readTimeout(30, TimeUnit.SECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .build();
 
     private RetrofitClient() {
         retrofit = new Retrofit.Builder()
                 .baseUrl(URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
                 .build();
     }
 
