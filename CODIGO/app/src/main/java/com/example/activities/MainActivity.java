2152f497e24f1a7573a32580283ee8505c54b069
germanleonelvianna <germanvianna95@gmail.com>
2020-06-04 00:02:47 -0300

    cambios a main activity

--- CODIGO/app/src/main/java/com/example/activities/MainActivity.java
+++ CODIGO/app/src/main/java/com/example/activities/MainActivity.java
@@ -37,8 +37,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SharedPreferences sharedPreferences = getSharedPreferences("PREFS", MODE_PRIVATE);
-        sharedPreferences.edit().remove("list data").commit();
         email = (EditText) findViewById(R.id.emailField);
         password = (EditText) findViewById(R.id.passwordField);
         login = (Button) findViewById(R.id.loginButton);
